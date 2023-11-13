5f379351affc9cdfe72b103e78e208af14b77edf
Buranovv <137188186+Buranovv@users.noreply.github.com>
2023-11-13 20:10:56 +0500

    done

--- src/components/list/CartList.tsx
+++ src/components/list/CartList.tsx
@@ -2,13 +2,18 @@
 
 import useCart from "@/zustand/cart";
 import CartCard from "../card/cart/cartCard";
+import { useEffect, useState } from "react";
+import UniversalData from "@/types/universalData";
 
 const CartList = () => {
   const { cart } = useCart();
-
+  const [data, setData] = useState<UniversalData[]>([]);
+  useEffect(() => {
+    setData(cart);
+  }, [cart]);
   return (
     <div className="cartList">
-      {cart.map((pr) => (
+      {data.map((pr) => (
         <CartCard key={pr._id} {...pr} />
       ))}
     </div>
