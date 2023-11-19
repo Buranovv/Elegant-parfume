80edc7cecb382572660298fe6179cefb6eb0d43b
Buranovv <137188186+Buranovv@users.noreply.github.com>
2023-11-20 02:16:57 +0500

    done

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -130,96 +130,94 @@ const ProductsPage = () => {
 
   return (
     <main>
-          <Box
-            sx={{
-              "& > :not(style)": { m: 1, width: "25ch" },
-            }}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <h2 style={{ width: 150 }}>Products ({total})</h2>
-            <div className="inputBox">
-              <TextField
-                id="outlined-basic"
-                label="Search"
-                variant="outlined"
-                value={search}
-                style={{ width: "100%" }}
-                onChange={(e) => handleSearch(e)}
-              />
-              <SearchIcon />
-            </div>
-            <Button
-              variant="contained"
-              style={{ width: 100 }}
-              endIcon={<AddIcon />}
-              onClick={() => showModal(reset)}
-            >
-              Add
-            </Button>
-          </Box>
-          {loading ? (
-            <Loader />
-          ) : (
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Image</StyledTableCell>
-                  <StyledTableCell align="center">Title</StyledTableCell>
-                  <StyledTableCell align="center">Price</StyledTableCell>
-                  <StyledTableCell align="center">Quantity</StyledTableCell>
-                  <StyledTableCell align="center">Category</StyledTableCell>
-                  <StyledTableCell align="center">Actions</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {allData?.map((row: UniversalData, i) => (
-                  <StyledTableRow
-                    key={i}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <StyledTableCell component="th" scope="row">
-                      <Image
-                        src={row.image?.url ?? ""}
-                        width={50}
-                        height={50}
-                        alt="sdsd"
-                        priority
-                      />
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.title}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.price} so`m
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.quantity}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.category?.name}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      <EditIcon
-                        className="edit"
-                        onClick={() => handleEdit(row._id)}
-                      />
-                      <DeleteIcon
-                        className="delete"
-                        onClick={() => deleteData(row._id)}
-                      />
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          )}
-      
+      <Box
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h2 style={{ width: 150 }}>Products ({total})</h2>
+        <div className="inputBox">
+          <TextField
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            value={search}
+            style={{ width: "100%" }}
+            onChange={(e) => handleSearch(e)}
+          />
+          <SearchIcon />
+        </div>
+        <Button
+          variant="contained"
+          style={{ width: 100 }}
+          endIcon={<AddIcon />}
+          onClick={() => showModal(reset)}
+        >
+          Add
+        </Button>
+      </Box>
+      {loading ? (
+        <Loader />
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Image</StyledTableCell>
+                <StyledTableCell align="center">Title</StyledTableCell>
+                <StyledTableCell align="center">Price</StyledTableCell>
+                <StyledTableCell align="center">Quantity</StyledTableCell>
+                <StyledTableCell align="center">Category</StyledTableCell>
+                <StyledTableCell align="center">Actions</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allData?.map((row: UniversalData, i) => (
+                <StyledTableRow
+                  key={i}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <StyledTableCell component="th" scope="row">
+                    <Image
+                      src={row.image?.url ?? ""}
+                      width={50}
+                      height={50}
+                      alt="sdsd"
+                      priority
+                    />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{row.title}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {row.price} so`m
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {row.quantity}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {row.category?.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <EditIcon
+                      className="edit"
+                      onClick={() => handleEdit(row._id)}
+                    />
+                    <DeleteIcon
+                      className="delete"
+                      onClick={() => deleteData(row._id)}
+                    />
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+
       <div>
         {!loading && total > LIMIT ? (
           <Pagination
